42ef3505d8e980691ab68b25e378476995319d14
Jakob__init__ <jsuetfels@gmail.com>
2024-08-03 22:10:35 +0200

    Update main.lua^

--- main.lua
+++ main.lua
@@ -1,23 +1,23 @@
-
 function love.load()
-    -- Window setup
-    love.window.setTitle("Simple Basketball Game")
+    love.window.setTitle("Advanced Basketball Game")
     love.window.setMode(800, 600, {resizable=false})
 
     white = {1, 1, 1}
     red = {1, 0, 0}
     green = {0, 1, 0}
-
+    black = {0, 0, 0}
+    
     ball = {
         x = 400,
         y = 300,
         radius = 15,
         dx = 0,
         dy = 0,
-        speed = 300
+        speed = 300,
+        gravity = 800,
+        jumpStrength = -400
     }
     
-
     hoop = {
         x = 700,
         y = 100,
@@ -25,12 +25,23 @@ function love.load()
         height = 10
     }
     
-
+    backboard = {
+        x = hoop.x - 10,
+        y = hoop.y - 50,
+        width = 10,
+        height = 100
+    }
+    
     score = 0
+    isJumping = false
+
+    sounds = {
+        score = love.audio.newSource("score.wav", "static"),
+        hit = love.audio.newSource("hit.wav", "static")
+    }
 end
 
 function love.update(dt)
-
     if love.keyboard.isDown("left") then
         ball.dx = -ball.speed
     elseif love.keyboard.isDown("right") then
@@ -39,37 +50,58 @@ function love.update(dt)
         ball.dx = 0
     end
     
-    if love.keyboard.isDown("up") then
-        ball.dy = -ball.speed
-    elseif love.keyboard.isDown("down") then
-        ball.dy = ball.speed
-    else
-        ball.dy = 0
+    if love.keyboard.isDown("space") and not isJumping then
+        ball.dy = ball.jumpStrength
+        isJumping = true
     end
     
+    ball.dy = ball.dy + ball.gravity * dt
     ball.x = ball.x + ball.dx * dt
     ball.y = ball.y + ball.dy * dt
     
+    if ball.y + ball.radius > love.graphics.getHeight() then
+        ball.y = love.graphics.getHeight() - ball.radius
+        ball.dy = 0
+        isJumping = false
+    end
 
     if ball.x + ball.radius > hoop.x and ball.x - ball.radius < hoop.x + hoop.width and
        ball.y + ball.radius > hoop.y and ball.y - ball.radius < hoop.y + hoop.height then
         score = score + 1
-        -- Reset ball position
+        sounds.score:play()
         ball.x = 400
         ball.y = 300
+        ball.dy = 0
+        isJumping = false
+    end
+    
+    if ball.x + ball.radius > backboard.x and ball.x - ball.radius < backboard.x + backboard.width and
+       ball.y + ball.radius > backboard.y and ball.y - ball.radius < backboard.y + backboard.height then
+        ball.dx = -ball.dx
+        sounds.hit:play()
     end
 end
 
 function love.draw()
-
+    love.graphics.setColor(black)
+    love.graphics.rectangle("fill", backboard.x, backboard.y, backboard.width, backboard.height)
+    
     love.graphics.setColor(red)
     love.graphics.rectangle("fill", hoop.x, hoop.y, hoop.width, hoop.height)
     
-
     love.graphics.setColor(green)
     love.graphics.circle("fill", ball.x, ball.y, ball.radius)
     
-
     love.graphics.setColor(white)
     love.graphics.print("Score: " .. score, 10, 10)
+end
+
+function love.keypressed(key)
+    if key == "r" then
+        score = 0
+        ball.x = 400
+        ball.y = 300
+        ball.dy = 0
+        isJumping = false
+    end
 end
